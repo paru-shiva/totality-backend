2196e3270437d3c796b90486087194ede66c6fbf
shiva <shivamotog62@gmail.com>
2024-07-31 21:57:30 +0530

    fc

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 import express from "express";
+import cors from "cors";
+import mysql from "mysql2/promise";
 const app = express();
+
+app.use(cors());
+
+const connection = await mysql.createConnection({
+  host: "bujtuwkrt3getswhsi8t-mysql.services.clever-cloud.com",
+  user: "uy5bvzwclj463gnm",
+  password: "FPKKauQCTP3mwKY6hFvL",
+  database: "bujtuwkrt3getswhsi8t",
+});
+
+try {
+  const [results] = await connection.query("show tables");
+  console.log(results);
+} catch (err) {
+  console.log(err);
+}
 
 app.get("/", (req, res) => {
   res.send("hellooooo");
