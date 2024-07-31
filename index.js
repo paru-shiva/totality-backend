0cd00396a5eded5f0466d0c86ceb29bb852f93b6
shiva <shivamotog62@gmail.com>
2024-07-31 19:00:27 +0530

    sc

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hellooooo");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
