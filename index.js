7ece27e6e5a0fabcb5b33ecd07463412f1ee8ebf
shiva <shivamotog62@gmail.com>
2024-08-03 11:02:50 +0530

    freedbbb

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ app.use(cors());
 app.use(express.json());
 
 const connection = await mysql.createConnection({
-  host: "bujtuwkrt3getswhsi8t-mysql.services.clever-cloud.com",
+  host: "sql.freedb.tech",
+  user: "freedb_parushiva",
+  database: "freedb_myfreedbfortest",
+  password: "y@fNNr9eRDq3fy9" /*
+
+  /*host: "bujtuwkrt3getswhsi8t-mysql.services.clever-cloud.com",
   user: "uy5bvzwclj463gnm",
   password: "FPKKauQCTP3mwKY6hFvL",
   database: "bujtuwkrt3getswhsi8t",
-  port: "3306" /*
+  port: "3306" */ /*
   host: "myawsdb.cpskmwc6eopp.eu-north-1.rds.amazonaws.com",
   user: "admin",
   database: "bujtuwkrt3getswhsi8t",
